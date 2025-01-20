7e2ec4e84e961c515f001ca6a9bef8372636891a
Sean Duggan <sean.duggan@gmail.com>
2025-01-20 01:33:51 -0500

    Further interactions with doors including emissive textures

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof(Animator))]
@@ -9,11 +11,42 @@ public class DoorScript : MonoBehaviour
     [SerializeField] bool _isOperable = true;
     [SerializeField] string _cannotOperateMessage = "Can't do that, boss";
 
+    [SerializeField] [ColorUsage(true, true)] Color _operableColor = Color.green * 2;
+    [SerializeField] [ColorUsage(true, true)] Color _inOperableColor = Color.red * 2;
+
+    [SerializeField] private List<Material> _emissionMaterials;
+
+    [SerializeField] GameObject _emissionObject;
+
     private void Awake()
     {
         _animator = GetComponent<Animator> ();
         Assert.IsNotNull(_animator);
         _isOpenHash = Animator.StringToHash("IsOpen");
+
+        if (_emissionObject && _emissionObject.TryGetComponent<MeshRenderer>(out MeshRenderer meshRenderer))
+        {
+            foreach (Material mat in meshRenderer.materials)
+            {
+                if (mat.IsKeywordEnabled("_EMISSION"))
+                {
+                    _emissionMaterials.Add(mat);
+                }
+            }
+        }
+
+        UpdateEmissionColor();
+    }
+
+    private void UpdateEmissionColor()
+    {
+        Debug.Log("Should be changing the emission colors...");
+        foreach (Material mat in _emissionMaterials)
+        {
+            Color newColor = _isOperable ? _operableColor : _inOperableColor;
+            Debug.Log($"Setting {mat.name} to {newColor}");
+            mat.SetColor("_EmissionColor", _isOperable ? _operableColor : _inOperableColor);
+        }
     }
 
     public void OperateDoor(bool isOpen)
@@ -43,5 +76,7 @@ public class DoorScript : MonoBehaviour
     public void SetDoorOperable(bool isOperable)
     {
         _isOperable = isOperable;
+
+        UpdateEmissionColor();
     }
 }
