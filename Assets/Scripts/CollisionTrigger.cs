239b743947e0d8da9aa08186d5392677fa67f4a0
Sean Duggan <sean.duggan@gmail.com>
2025-01-21 08:19:45 -0500

    Updated the weights of things to make them work

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -4,9 +4,17 @@ using UnityEngine.Events;
 public class CollisionTrigger : MonoBehaviour
 {
     [SerializeField] UnityEvent _onDepressed;
+    [SerializeField] UnityEvent _onReleased;
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log($"Depressed by {collision.gameObject.name}");
         _onDepressed?.Invoke();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log($"Released by {collision.gameObject.name}");
+        _onReleased?.Invoke();
     }
 }
